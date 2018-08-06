8e66619e2637bc6d1698ee712bea590abf3a8892
Kris <31852063+krisfris@users.noreply.github.com>
2018-08-06 14:58:57 +0200

    Add "open with" menu (close #6)

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ziu
 File manager
+
+## Requirements
+apt install libmagic1
+
+## Notes
+
+### File type recognition
+ziu defines file type by looking at the filename extension and
+python-magic for filenames without extensions.
+
+This is only relevant if an item is opened through the "open with" menu
+as xdg-open will be used otherwise.
+
+https://github.com/h2non/filetype.py may be another good option
+especially for cross-platform.
--- ziu/main.py
+++ ziu/main.py
@@ -2,16 +2,33 @@ import os
 import sys
 import subprocess
 import traceback
+import appdirs
+import magic
 from collections import namedtuple
 from operator import attrgetter
 from send2trash import send2trash
+from dieselhaze.db_util import db_connect
 
 from ziu.qt import *
 
 
 rootdir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.abspath(__file__))))
 mw = None
+datadir = appdirs.user_data_dir('ziu')
+os.makedirs(datadir, exist_ok=True)
+con = db_connect(os.path.join(datadir, 'ziu.db'))
 
+
+with con:
+    con.cursor().execute(
+        'create table if not exists openwith ('
+        'id integer primary key,'
+        'name text,'
+        'ext text,'
+        'cmd text,'
+        'mime text,'
+        'isdir integer'
+    ')')
 
 class LocationHistory:
     def __init__(self, initial_path):
@@ -60,23 +77,36 @@ class LocationHistory:
 folder_icon = QIcon(os.path.join(rootdir, 'pics/icons/tango/scalable/places/folder.svg'))
 file_icon = QIcon(os.path.join(rootdir, 'pics/icons/tango/scalable/mimetypes/text-x-generic.svg'))
 
-FolderItem = namedtuple('FolderItem', ['basename', 'basename_lower', 'path', 'isdir', 'icon'])
+
+class FolderItem:
+    def __init__(self, direntry):
+        self.direntry = direntry
+        self.path = direntry.path
+        self.name = direntry.name
+
+    @property
+    def name_lower(self):
+        return self.name.lower()
+
+    @property
+    def isdir(self):
+        return self.direntry.is_dir(follow_symlinks=False)
+
+    @property
+    def icon(self):
+        return folder_icon if self.isdir else file_icon
+
+    @property
+    def size(self):
+        return self.direntry.stat(follow_symlinks=False).st_size
 
 
 def get_folder_content(loc):
     try:
-        filenames = [x.name for x in os.scandir(loc)]
+        for entry in os.scandir(loc):
+            yield FolderItem(entry)
     except PermissionError:
         QMessageBox.critical(mw, 'Permission denied', 'Could not access location.')
-        return
-    for filename in filenames:
-        path = os.path.join(loc, filename)
-        isdir = os.path.isdir(path)
-        if isdir:
-            icon = folder_icon
-        else:
-            icon = file_icon
-        yield FolderItem(filename, filename.lower(), path, isdir, icon)
 
 
 def get_home():
@@ -96,7 +126,7 @@ class FolderModel(QAbstractTableModel):
             self.items = []
         else:
             files = get_folder_content(self.loc)
-            self.items = sorted(sorted(files, key=attrgetter('basename_lower')),
+            self.items = sorted(sorted(files, key=attrgetter('name_lower')),
                                 key=attrgetter('isdir'), reverse=True)
         self.layoutChanged.emit()
 
@@ -109,7 +139,7 @@ class FolderModel(QAbstractTableModel):
     def data(self, index, role):
         item = self.items[index.row()]
         if role == Qt.DisplayRole:
-            return item.basename
+            return item.name
         elif role == Qt.DecorationRole:
             return item.icon
         return QVariant()
@@ -125,6 +155,23 @@ class FolderModel(QAbstractTableModel):
 
     def get_item(self, index):
         return self.items[index.row()]
+
+
+class OpenWithDialog(QDialog):
+    def __init__(self, parent):
+        super(OpenWithDialog, self).__init__(parent)
+        self.ui = loadUi('designer/openwith.ui', self)
+        self.enable_ok(False)
+        self.ui.openwith_cmd_edit.textChanged.connect(self.cmd_changed)
+
+    def enable_ok(self, enable):
+        self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(enable)
+
+    def cmd_changed(self, value):
+        if value:
+            self.enable_ok(True)
+        else:
+            self.enable_ok(False)
 
 
 class MainWindow(QMainWindow):
@@ -160,21 +207,69 @@ class MainWindow(QMainWindow):
         self.ui.actionZoomOut.triggered.connect(self.zoom_out)
         self.ui.actionNormalSize.triggered.connect(self.normal_size)
         self.ui.actionAbout.triggered.connect(self.about)
+        self.ui.actionOpenWith.triggered.connect(self.open_with_triggered)
 
         QShortcut(QKeySequence('Ctrl+L'), self, self.edit_location)
         QShortcut(QKeySequence('Return'), self, self.enter_pressed)
         QShortcut(QKeySequence('Enter'), self, self.enter_pressed)
         QShortcut(QKeySequence('Del'), self, self.del_pressed)
         QShortcut(QKeySequence('F4'), self, self.f4_pressed)
+        QShortcut(QKeySequence('Ctrl+W'), self, QApplication.quit)
 
         self.ui.listView.doubleClicked.connect(self.open_selected)
         self.ui.listView.filter_triggered.connect(self.filter_triggered)
+        self.ui.listView.customContextMenuRequested.connect(self.listview_contextmenu_requested)
 
         self.current_icon_size = self.default_icon_size
 
         self.ui.edit_filter.setVisible(False)
         self.ui.edit_filter.close_filter.connect(self.close_filter)
         self.ui.edit_filter.textChanged.connect(self.filter_changed)
+
+    def listview_contextmenu_requested(self, pos):
+        globalpos = self.ui.listView.mapToGlobal(pos)
+        index = self.ui.listView.indexAt(pos)
+        item = self.ui.listView.model().get_item(index)
+        _, ext = os.path.splitext(item.path)
+        if index.row() != -1:
+            menu = QMenu()
+            openwith_menu = menu.addMenu('Open with')
+            if item.isdir:
+                apps = con.cursor().execute('select * from openwith where isdir = 1')
+            elif ext:
+                apps = con.cursor().execute('select * from openwith where ext = ?', (ext,))
+            else:
+                mime = magic.from_file(item.path)
+                apps = con.cursor().execute('select * from openwith where mime = ?', (mime,))
+            apps = apps.fetchall()
+            app = None
+            for app in apps:
+                action = QAction(app['name'], self)
+                action.triggered.connect(lambda: self.open_file_custom(app['cmd'], item.path))
+                openwith_menu.addAction(action)
+
+            if app is not None:
+                openwith_menu.addSeparator()
+                
+            self.ui.actionOpenWith.setData(index)
+            openwith_menu.addAction(self.ui.actionOpenWith)
+
+            menu.exec_(globalpos)
+
+    def open_with_triggered(self):
+        dialog = OpenWithDialog(self)
+        if dialog.exec_():
+            index = self.sender().data()
+            item = self.ui.listView.model().get_item(index)
+            cmd = dialog.openwith_cmd_edit.text()
+            self.open_file_custom(cmd, item.path)
+            name = dialog.openwith_name_edit.text()
+            if name:
+                _, ext = os.path.splitext(item.path)
+                mime = None if item.isdir else magic.from_file(item.path)
+                with con:
+                    con.cursor().execute('insert into openwith (name, ext, cmd, mime, isdir) values (?, ?, ?, ?, ?)',
+                        (name, ext, cmd, mime, item.isdir))
 
     def filter_triggered(self, text):
         self.edit_filter.setText(text)
@@ -190,7 +285,7 @@ class MainWindow(QMainWindow):
         self.listView.selectionModel().clearSelection()
         if not value:
             return
-        rows = [x[0] for x in enumerate(self.foldermodel.items) if x[1].basename_lower.startswith(value.lower())]
+        rows = [x[0] for x in enumerate(self.foldermodel.items) if x[1].name_lower.startswith(value.lower())]
         if rows:
             index = self.foldermodel.index(rows[0], 0)
             self.listView.selectionModel().select(index, QItemSelectionModel.Select)
@@ -199,6 +294,12 @@ class MainWindow(QMainWindow):
     def edit_location(self):
         self.ui.locationEdit.selectAll()
         self.ui.locationEdit.setFocus()
+
+    def open_file_custom(self, cmd, path):
+        try:
+            subprocess.Popen([cmd, path])
+        except:
+            QMessageBox.critical(self, 'Error opening file', traceback.format_exc())
 
     def open_file(self, path):
         QDesktopServices.openUrl(QUrl(path))
